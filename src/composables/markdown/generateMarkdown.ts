6007bb81c35c0269747ab632fff35229b5a94ad2
sqsuqi <643352633@qq.com>
2024-12-24 16:33:58 +0800

    refactor(markdown): 简化文件处理逻辑，优化代码可读性

--- src/composables/markdown/generateMarkdown.ts
+++ src/composables/markdown/generateMarkdown.ts
@@ -6,26 +6,24 @@ export function generateMarkdown() {
   const shortsDir = './pages/shorts'
 
   // 先清除除了 index.md 外的所有文件
-  fs.readdir(shortsDir)
-    .then((files) => {
-      files.forEach((file) => {
-        // 保留 index.md，删除其他文件
-        if (file !== 'index.md') {
-          const filePath = path.join(shortsDir, file)
-          fs.remove(filePath)
-            .then(() => {
-              // eslint-disable-next-line no-console
-              console.log(`✅ Deleted old file: ${filePath}`)
-            })
-            .catch((err) => {
-              console.error(`❌ Error deleting file ${filePath}:`, err)
-            })
-        }
-      })
+  fs.readdir(shortsDir).then((files) => {
+    files.forEach((file) => {
+      // 保留 index.md，删除其他文件
+      if (file !== 'index.md') {
+        const filePath = path.join(shortsDir, file)
+        fs.remove(filePath)
+          .then(() => {
+            // eslint-disable-next-line no-console
+            console.log(`✅ Deleted old file: ${filePath}`)
+          })
+          .catch((err) => {
+            console.error(`❌ Error deleting file ${filePath}:`, err)
+          })
+      }
     })
-    .catch((err) => {
-      console.error('❌ Error reading directory:', err)
-    })
+  }).catch((err) => {
+    console.error('❌ Error reading directory:', err)
+  })
 
   shorts.forEach((item, index) => {
     const { title, date, imgURl } = item
@@ -46,21 +44,19 @@ ${title}
     // 确保 public 目录存在
     const filePath = `./pages/shorts/shorts${index + 1}.md`
 
-    fs.ensureDir('./pages/shorts')
-      .then(() => {
-        // 写入每个 Markdown 文件
-        fs.writeFile(filePath, markdownContent, (err) => {
-          if (err) {
-            console.error(`❌ Error creating file ${filePath}:`, err)
-          }
-          else {
-            // eslint-disable-next-line no-console
-            console.log(`✅ Markdown file successfully created: ${filePath}`)
-          }
-        })
+    fs.ensureDir('./pages/shorts').then(() => {
+      // 写入每个 Markdown 文件
+      fs.writeFile(filePath, markdownContent, (err) => {
+        if (err) {
+          console.error(`❌ Error creating file ${filePath}:`, err)
+        }
+        else {
+          // eslint-disable-next-line no-console
+          console.log(`✅ Markdown file successfully created: ${filePath}`)
+        }
       })
-      .catch((err) => {
-        console.error('❌ Error ensuring directory:', err)
-      })
+    }).catch((err) => {
+      console.error('❌ Error ensuring directory:', err)
+    })
   })
 }
