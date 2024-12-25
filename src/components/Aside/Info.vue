750322cbe895475b2fbafcba3dc6307252e09fad
sqsuqi <643352633@qq.com>
2024-12-25 12:56:58 +0800

    refactor(aside): 重构Info.vue组件内容和样式

--- src/components/Aside/Info.vue
+++ src/components/Aside/Info.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div class="mb-2 rounded-lg p-6 aside">
     <div class="mb-2 flex items-center justify-center">
@@ -8,13 +5,10 @@
     </div>
 
     <h2 class="mb-2 text-2xl font-bold">
-      关于我们
+      关于我
     </h2>
     <p class="text-gray-600 dark:text-gray-300">
-      欢迎来到我们的博客！我们专注于分享最新的技术资讯与开发教程。
+      欢迎来到我的博客！专注于分享最新的技术资讯与开发教程。
     </p>
   </div>
 </template>
-
-<style scoped>
-</style>
