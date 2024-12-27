64cf10bb3089dc82212b1b3651123307c4d5567b
sqsuqi <643352633@qq.com>
2024-12-27 11:59:59 +0800

    feat: 添加RSS生成脚本及更新依赖项和构建命令

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -109,6 +109,9 @@ watch(() => route.path, () => {
           <div class="i-carbon-paint-brush cursor-pointer font-size-2xl card-hover-text" @click="toggleShow()" />
           <UseTheme v-if="isShow" class="use-theme pos-absolute right-0 top-64px" />
         </li>
+        <li class="flex items-center">
+          <a i-la-rss-square cursor-pointer font-size-2xl card-hover-text href="/feed.xml" target="_blank" />
+        </li>
       </ul>
 
       <!-- Mobile Menu Button (Hamburger) -->
